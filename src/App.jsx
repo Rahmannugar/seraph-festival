12be6690a04d8c1663452a2dd46d49304d287777
Nugar <cladeadenugar@gmail.com>
2023-10-15 05:48:15 +0100

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 const Home = React.lazy(() => import("./Pages/Home"));
 const EventPlan = React.lazy(() => import("./Pages/EventPlan"));
@@ -8,14 +8,12 @@ const Registration = React.lazy(() => import("./Pages/Registration"));
 const App = () => {
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/event" element={<EventPlan />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/register" element={<Registration />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/event" element={<EventPlan />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Registration />} />
+      </Routes>
     </div>
   );
 };
